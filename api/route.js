9dbd61b7b8a68b58fbd878b4211d8a77dae5771a
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-09-27 21:51:40 +0100

    Update

--- api/route.js
+++ api/route.js
@@ -3,11 +3,6 @@ const router = new Router();
 const usersModel = require("../models/users");
 
 
-usersModel.find({}).then((users) => {
-  console.log(users);
-});
-
-
 //visit http://localhost:3000/api/
 router.basePath = "/";
 
