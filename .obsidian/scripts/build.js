aa9a734015f98bc8148d8cd01b4be399fc14894c
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-11-12 18:28:37 +0100

    Update the buld engine

--- .obsidian/scripts/build.js
+++ .obsidian/scripts/build.js
@@ -1,5 +1,8 @@
+const path = require("path");
 const { Build, COLORS } = require("../../modules");
-const { execSync } = require("child_process");
+const { execSync, spawn } = require("child_process");
+
+const buildPath = path.join(process.cwd(), "dist");
 const logger = (message) =>
   console.log(
     COLORS.GREEN_TEXT +
@@ -13,8 +16,28 @@ const builder = new Build();
 builder.buildAllPages();
 builder.buildALLStyles();
 builder.buildAllScripts();
+builder.buildAllAssets();
+builder.buildAllPublic();
+builder.setUpNewServer();
 let end = new Date().getTime();
 logger("Build Completed in " + (end - start) + "ms");
-const process = execSync("node ./.obsidian/workers/server/index.js", {
-  stdio: "inherit",
-});
+try {
+  let args = ["index.js"];
+  let process = spawn("node", args, { cwd: buildPath });
+  process.stdout.on("data", (data) => {
+    logger(data.toString());
+  });
+
+  process.stderr.on("data", (data) => {
+    logger(data.toString());
+  });
+
+  process.on("error", function (error) {
+    logger(error);
+  });
+  process.on("exit", function (code, signal) {
+    logger("Build process exited with code: " + code);
+  });
+} catch (error) {
+  console.log(error);
+}
